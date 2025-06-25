88f051ff33da1722ade62cf4269a0c8058cd83a2
Irihon <rudasya15@gmail.com>
2025-06-25 20:11:39 +0300

    Your second commit message

--- about.php
+++ about.php
@@ -28,6 +28,9 @@ $bg_image = get_theme_mod('about_page_bg_image');
 
     if (!empty($about_story_content || $about_story_image)): ?>
         <section class="about__main-section">
+            <div>
+                <img class="about__main-section__stiker" src="/wp-content/themes/foodzero/src/assets/images/about-story-stiker.png" alt="">
+            </div>
             <div class="grid-container">
                 <div class="grid-x grid-margin-x align-bottom">
                     <div class="cell small-12 medium-6">
@@ -107,6 +110,65 @@ $bg_image = get_theme_mod('about_page_bg_image');
         </section>
     <?php endif ?>
 
-    
+    <?php
+    $about_process_title = get_field('about_process_title');
+    $about_process_steps_1 = get_field('about_process_steps_1');
+    $about_process_steps_2 = get_field('about_process_steps_2');
+    $about_process_steps_3 = get_field('about_process_steps_3');
+
+    if (!empty($about_process_title || $about_process_steps_1 || $about_process_steps_2 || $about_process_steps_3)): ?>
+        <section class="about__process-section">
+            <div class="about__process-section__stikers">
+                <img class="about__process-section__stikers-1" src="/wp-content/themes/foodzero/src/assets/images/about-process-stiker-1.png" alt="">
+                <img class="about__process-section__stikers-2" src="/wp-content/themes/foodzero/src/assets/images/about-process-stiker-2.png" alt="">
+            </div>
+            <div class="grid-container">
+                <div class="grid-x grid-margin-x text-center">
+                    <div class="cell">
+                        <div class="about__process-section__title">
+                            <?php echo ($about_process_title) ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="grid-x grid-margin-x">
+                    <div class="cell small-12 medium-6">
+                        <div class="about__process-section__left-column">
+                            <div class="about__process-section__image">
+                                <img src="<?php echo esc_url($about_process_steps_1['about_process_steps_image']['url']); ?>"
+                                    alt="<?php echo esc_attr($about_process_steps_1['about_process_steps_image']['alt']); ?>">
+                            </div>
+                            <div class="about__process-section__content">
+                                <?php echo ($about_process_steps_3['about_process_steps_content']); ?>
+                            </div>
+                            <div class="about__process-section__image">
+                                <img src="<?php echo esc_url($about_process_steps_3['about_process_steps_image']['url']); ?>"
+                                    alt="<?php echo esc_attr($about_process_steps_3['about_process_steps_image']['alt']); ?>">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="cell small-12 medium-6">
+                        <div class="about__process-section__right-column">
+                            <div class="about__process-section__content">
+                                <?php echo ($about_process_steps_1['about_process_steps_content']); ?>
+                            </div>
+                            <div class="about__process-section__image">
+                                <img src="<?php echo esc_url($about_process_steps_2['about_process_steps_image']['url']); ?>"
+                                    alt="<?php echo esc_attr($about_process_steps_2['about_process_steps_image']['alt']); ?>">
+                            </div>
+                            <div class="about__process-section__content right-bottom-content">
+                                <?php echo ($about_process_steps_2['about_process_steps_content']); ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    <?php endif; ?>
+
+    <?php get_template_part('template-parts/section', 'reservation');
+    ?>
+
+
 </div>
 <?php get_footer() ?>
